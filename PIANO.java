65f59e5da118adb598e9a1f0ff8aa50568852063
Shamim-Khan <134519275+shamimkhan8134@users.noreply.github.com>
2024-12-01 21:39:02 +0600

    Add files via upload

---
+++ PIANO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.piano;
+
+/**
+ *
+ * @author shamim hossain
+ */
+public class PIANO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
